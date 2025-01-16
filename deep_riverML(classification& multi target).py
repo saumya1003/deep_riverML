24c32cdd61b0e0df24e899d03b57f78e21b979a8
saumya1003 <108856999+saumya1003@users.noreply.github.com>
2025-01-16 16:03:22 +0530

    Add files via upload

---
+++ deep_riverML(classification& multi target).py
@@ -0,0 +1,8 @@
+from river.ensemble import AdaptiveRandomForestClassifier
+print(AdaptiveRandomForestClassifier)
+
+from river import ensemble
+print(dir(ensemble))
+ 
+from river import ensemble
+print(AdaptiveRandomForestClassifier)
